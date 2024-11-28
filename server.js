0d31d547d334e59f1ca93962c5ba814f36cfb1a9
chatdobicho <chatdobicho@gmail.com>
2024-11-28 15:47:51 -0300

    Adição do token

--- server.js
+++ server.js
@@ -7,6 +7,21 @@ app.get('/webhook', (req, res) => {
     res.status(200).send('Webhook funcionando!');
 });
 
+app.get('/webhook', (req, res) => {
+    const VERIFY_TOKEN = 'Chatdobicho2206*';
+
+    const mode = req.query['hub.mode'];
+    const token = req.query['hub.verify_token'];
+    const challenge = req.query['hub.challenge'];
+
+    if (mode && token === VERIFY_TOKEN) {
+        res.status(200).send(challenge);
+    } else {
+        res.status(403).send('Falha na validação');
+    }
+});
+
+
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 });
